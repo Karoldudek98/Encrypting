f2b8c3970d2272ed0173063bdae46dded8f12715
Karol Dudek <karol.dudek@microsoft.wsei.edu.pl>
2024-01-22 23:02:12 +0100

    Add project files.

---
+++ Encrypting/Pages/Caesar.cshtml.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text;
+
+namespace Encrypting.Pages
+{
+    public class CaesarModel : PageModel
+    {
+        [BindProperty]
+        public string InputText { get; set; }
+
+        [BindProperty]
+        public int Shift { get; set; }
+
+        public string EncryptedText { get; private set; }
+
+        public void OnPost()
+        {
+            EncryptedText = EncryptCaesar(InputText, Shift);
+        }
+
+        private string EncryptCaesar(string input, int shift)
+        {
+            string polishAlphabet = "a¹bcædeêfghijkl³mnñoópqrsœtuvwxyzŸ¿";
+
+            shift = shift % polishAlphabet.Length;
+
+            StringBuilder result = new StringBuilder();
+
+            foreach (char c in input)
+            {
+                if (char.IsLetter(c))
+                {
+                    string alphabet = char.IsUpper(c) ? polishAlphabet.ToUpper() : polishAlphabet;
+
+                    int index = alphabet.IndexOf(c);
+                    if (index != -1)
+                    {
+                        int shiftedIndex = (index + shift) % alphabet.Length;
+                        char shiftedChar = alphabet[shiftedIndex];
+                        result.Append(shiftedChar);
+                    }
+                    else
+                    {
+                        result.Append(c);
+                    }
+                }
+                else
+                {
+                    result.Append(c);
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
